805fb8fa6d8a13753d292014b87199251aae0463
Luke Thompson <endeavour9@gmail.com>
2019-11-23 22:53:55 +1100

    convert handler to typescript: tsc --init npm i -D @types/node @types/aws-lambda
    tsconfig.json: switch to target es2017 + add @types to typeRoots and types
    rename handler.js handler.ts
    add strong typing to handler.ts for the handler function
    ignore .js files in gitignore

--- handler.ts
+++ handler.ts
@@ -1,6 +1,8 @@
 'use strict';
 
-module.exports.hello = async event => {
+import {APIGatewayProxyHandler} from "aws-lambda";
+
+export const hello: APIGatewayProxyHandler = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
