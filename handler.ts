063a0a492e28ab241447fb726509d13fd4e88cd1
Luke Thompson <endeavour9@gmail.com>
2019-11-24 00:25:35 +1100

    serve an index.html file on the root path and a JSON response on all other paths

--- handler.ts
+++ handler.ts
@@ -1,20 +1,41 @@
 'use strict';
 
 import {APIGatewayProxyHandler} from "aws-lambda";
+import * as fs from "fs";
 
 export const http: APIGatewayProxyHandler = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    //normalise the path no matter whether the request is on the API Gateway URL or the api.mycodefu.com URL
+    if (event.path.startsWith("/aws-lambda-app")) {
+        event.path = event.path.substr("/aws-lambda-app".length);
+    }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    if (event.path === "/" || event.path === "/index.html") {
+        //serve index.html on the root / or /index.html
+        let indexHtmlBuffer = fs.readFileSync("./index.html");
+        let indexHtmlBase64 = new Buffer(indexHtmlBuffer).toString('base64');
+        return {
+            statusCode: 200,
+            headers: {
+                "Content-Type": "text/html"
+            },
+            isBase64Encoded: true,
+            body: indexHtmlBase64
+        };
+
+    } else {
+        return {
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(
+                {
+                    message: 'Go Serverless v1.0! Your function executed successfully!',
+                    input: event,
+                },
+                null,
+                2
+            ),
+        };
+    }
 };
