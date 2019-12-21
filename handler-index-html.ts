99512f70b7b3667d0acdc23914972b74602a1a86
Luke Thompson <endeavour9@gmail.com>
2019-12-22 09:58:42 +1100

    only read the index html on cold-start, from then on return from memory

--- handler-index-html.ts
+++ handler-index-html.ts
@@ -6,19 +6,23 @@ import {HttpEventHandler} from "./interfaces";
  * Serve index.html on the root / or /index.html.
  */
 export class IndexHtmlHandler implements HttpEventHandler {
+    private indexHtml: string = "";
+
     public canHandleThis(event: APIGatewayProxyEvent) {
         return event.path === "/" || event.path === "/index.html";
     }
 
     public async handle(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-        const indexHtml = fs.readFileSync("./index.html", {encoding: "utf8"});
+        if (!this.indexHtml) {
+            this.indexHtml = fs.readFileSync("./index.html", {encoding: "utf8"});
+        }
         return {
             statusCode: 200,
             headers: {
                 "Content-Type": "text/html",
             },
             isBase64Encoded: false,
-            body: indexHtml,
+            body: this.indexHtml,
         };
     }
 }
